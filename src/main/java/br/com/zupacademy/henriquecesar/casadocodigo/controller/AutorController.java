7c6269348f7f4a4ac6d41083716d45bb82bb2275
Henrique Cesar <henrique.jose@zup.com.br>
2021-09-27 12:34:13 -0300

    criação da classe AutorController

---
+++ src/main/java/br/com/zupacademy/henriquecesar/casadocodigo/controller/AutorController.java
@@ -0,0 +1,12 @@
+package br.com.zupacademy.henriquecesar.casadocodigo.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/autores")
+public class AutorController {
+
+}
