8c114a1665a8664a88bf1731d857313c550e32c6
Henrique Cesar <henrique.jose@zup.com.br>
2021-09-27 16:42:46 -0300

    adição dos métodos getters de AutorForm

--- src/main/java/br/com/zupacademy/henriquecesar/casadocodigo/form/AutorForm.java
+++ src/main/java/br/com/zupacademy/henriquecesar/casadocodigo/form/AutorForm.java
@@ -22,4 +22,16 @@ public class AutorForm {
 	public Autor toModel() {
 		return new Autor(this.nome, this.email, this.descricao, LocalDateTime.now());
 	}
+	
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
 }
