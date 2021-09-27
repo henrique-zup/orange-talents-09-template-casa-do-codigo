5deb5b44ebc8966a84f66f4295c306f1ad9b5313
Henrique Cesar <henrique.jose@zup.com.br>
2021-09-27 16:48:08 -0300

    integração do validator para proibir emails duplicados em AutorForm, no AutorController

--- src/main/java/br/com/zupacademy/henriquecesar/casadocodigo/controller/AutorController.java
+++ src/main/java/br/com/zupacademy/henriquecesar/casadocodigo/controller/AutorController.java
@@ -3,12 +3,15 @@ package br.com.zupacademy.henriquecesar.casadocodigo.controller;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.zupacademy.henriquecesar.casadocodigo.form.AutorForm;
+import br.com.zupacademy.henriquecesar.casadocodigo.form.validator.ProibeEmailDuplicadoValidator;
 import br.com.zupacademy.henriquecesar.casadocodigo.modelo.Autor;
 import br.com.zupacademy.henriquecesar.casadocodigo.repository.AutorRepository;
 
@@ -18,6 +21,14 @@ public class AutorController {
 	
 	@Autowired
 	private AutorRepository autorRepository;
+	
+	@Autowired
+	private ProibeEmailDuplicadoValidator proibeEmailDuplicadoValidator;
+	
+	@InitBinder
+	public void init(WebDataBinder binder) {
+		binder.addValidators(proibeEmailDuplicadoValidator);
+	}
 	
 	@PostMapping
 	public void cadastraAutor(@RequestBody @Valid AutorForm autorForm) {
