63eec26f263e9c945056c6a529f17db6c470efc7
Henrique Cesar <henrique.jose@zup.com.br>
2021-09-28 16:44:37 -0300

    remoção de validators antigos

--- src/main/java/br/com/zupacademy/henriquecesar/casadocodigo/controller/CategoriaController.java
+++ src/main/java/br/com/zupacademy/henriquecesar/casadocodigo/controller/CategoriaController.java
@@ -3,15 +3,12 @@ package br.com.zupacademy.henriquecesar.casadocodigo.controller;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.zupacademy.henriquecesar.casadocodigo.form.CategoriaForm;
-import br.com.zupacademy.henriquecesar.casadocodigo.form.validator.ProibeCategoriaDuplicadaValidator;
 import br.com.zupacademy.henriquecesar.casadocodigo.modelo.Categoria;
 import br.com.zupacademy.henriquecesar.casadocodigo.repository.CategoriaRepository;
 
@@ -21,14 +18,6 @@ public class CategoriaController {
     
     @Autowired
     private CategoriaRepository categoriaRepository;
-    
-    @Autowired
-    private ProibeCategoriaDuplicadaValidator proibeCategoriaDuplicadaValidator;
-    
-    @InitBinder
-    public void init(WebDataBinder binder) {
-        binder.addValidators(proibeCategoriaDuplicadaValidator);
-    }
     
     @PostMapping
     public void cadastraCategoria(@RequestBody @Valid CategoriaForm form) {
