9fb057c7bfe3bdf3296c9e4d206ce389c739df9f
Henrique Cesar <henrique.jose@zup.com.br>
2021-09-30 14:43:17 -0300

    criação do método para cadastrar um estado

--- src/main/java/br/com/zupacademy/henriquecesar/casadocodigo/controller/EstadoController.java
+++ src/main/java/br/com/zupacademy/henriquecesar/casadocodigo/controller/EstadoController.java
@@ -2,9 +2,16 @@ package br.com.zupacademy.henriquecesar.casadocodigo.controller;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+import javax.validation.Valid;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.zupacademy.henriquecesar.casadocodigo.form.EstadoForm;
+import br.com.zupacademy.henriquecesar.casadocodigo.modelo.Estado;
 
 @RestController
 @RequestMapping("/estados")
@@ -12,5 +19,12 @@ public class EstadoController {
     
     @PersistenceContext
     private EntityManager entityManager;
+    
+    @PostMapping
+    @Transactional
+    public void cadastraEstado(@RequestBody @Valid EstadoForm form) {
+        Estado estado = form.toModel(entityManager);
+        entityManager.persist(estado);        
+    }
 
 }
