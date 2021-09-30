6b1de6e98ba195f75716960cb52d40cd3912a1a5
Henrique Cesar <henrique.jose@zup.com.br>
2021-09-30 14:41:53 -0300

    criação do método para cadastrar um país

--- src/main/java/br/com/zupacademy/henriquecesar/casadocodigo/controller/PaisController.java
+++ src/main/java/br/com/zupacademy/henriquecesar/casadocodigo/controller/PaisController.java
@@ -2,9 +2,16 @@ package br.com.zupacademy.henriquecesar.casadocodigo.controller;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+import javax.validation.Valid;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.zupacademy.henriquecesar.casadocodigo.form.PaisForm;
+import br.com.zupacademy.henriquecesar.casadocodigo.modelo.Pais;
 
 @RestController
 @RequestMapping("/paises")
@@ -12,5 +19,12 @@ public class PaisController {
     
     @PersistenceContext
     private EntityManager entityManager;
+    
+    @PostMapping
+    @Transactional
+    public void cadastraPais(@RequestBody @Valid PaisForm form) {
+        Pais pais = form.toModel();
+        entityManager.persist(pais);        
+    }
 
 }
