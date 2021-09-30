2acc8da5d2c1d0ea25ddd38f9fc5024e0f497bd9
Henrique Cesar <henrique.jose@zup.com.br>
2021-09-30 08:57:27 -0300

    adição do método findAll no LivroRepository com o retorno do tipo List<Livro>

--- src/main/java/br/com/zupacademy/henriquecesar/casadocodigo/repository/LivroRepository.java
+++ src/main/java/br/com/zupacademy/henriquecesar/casadocodigo/repository/LivroRepository.java
@@ -1,5 +1,7 @@
 package br.com.zupacademy.henriquecesar.casadocodigo.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import br.com.zupacademy.henriquecesar.casadocodigo.modelo.Livro;
 
 @Repository
 public interface LivroRepository extends CrudRepository<Livro, Long> {
+    
+    List<Livro> findAll();
 
 }
